b5214719009ebd2f4319ce86b87eda78ab5f828f
Adam Smelko <adamsmelko1@gmail.com>
2023-02-10 13:50:29 +0100

    more tests

--- exastolog/test.py
+++ exastolog/test.py
@@ -3,11 +3,11 @@ from Simulation import Simulation
 from os.path import dirname, join
 
         
-# model = Model(join(dirname(__file__), "../notebooks/model_files/toy3.bnet"))
-# simulation = Simulation(model, ['A','B'], [0, 0])
-# result = simulation.get_last_states_probtraj()
+model = Model(join(dirname(__file__), "../notebooks/model_files/toy2.bnet"))
+simulation = Simulation(model, ['A','B', 'C'], [0, 0, 0])
+result = simulation.get_last_states_probtraj()
 
-model = Model("../notebooks/model_files/krasmodel15vars.bnet")
+model = Model(join(dirname(__file__), "../notebooks/model_files/krasmodel15vars.bnet"))
 sim = Simulation(model, ['cc','KRAS','DSB','cell_death'], [1, 1, 1, 0])
 result = sim.get_last_states_probtraj()
 print(result)
